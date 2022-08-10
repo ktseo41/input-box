db465a50d256024385ac1dd1ea574d54ed9344b5
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-08-10 21:04:34 +0900

    init

---
+++ src/InputBox.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, watch } from "vue";
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
+const inputValue = ref(props.modelValue);
+
+watch(inputValue, (value) => {
+  emit("update:modelValue", value);
+});
+</script>
+
+<template>
+  <input
+    v-model="inputValue"
+    type="text"
+    placeholder="할 일은 많고 시간은 없고.."
+  />
+</template>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import InputBox from "./InputBox.vue";
+
+createApp(InputBox).mount("#app");
